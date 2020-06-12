6efaa40acf06d2963ade4fe6208fbb052c53200e
thebaptiste <jean-baptiste.veslin@meteo.fr>
2020-06-12 15:44:54 +0000

    feat: first version with python2 integration tests from mfserv

---
+++ integration_tests/0004_noweb/test.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import os
+from mfutil import BashWrapperOrRaise
+
+NGINX_PORT = int(os.environ['MFSERV_NGINX_PORT'])
+
+#python2@mfext may not be installed
+#bash_wrapper = BashWrapperOrRaise("is_layer_installed python2@mfext")
+#if bash_wrapper.stdout != "1":
+#    templates = ("python3_noweb", "node_noweb")
+#else:
+    templates = ("python2_noweb")
+
+for template in templates:
+
+    BashWrapperOrRaise("rm -Rf foobar")
+    BashWrapperOrRaise("plugins.uninstall foobar || true")
+
+    print(BashWrapperOrRaise("bootstrap_plugin.py create --template=%s "
+                             "--no-input foobar" % template))
+    print(BashWrapperOrRaise("cd foobar && make release"))
+    print(BashWrapperOrRaise('cd foobar && plugins.install "$(ls *.plugin)"'))
+
+    print("ok")
